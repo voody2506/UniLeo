9178d74330881922c775c9b973d5e1fc0b66ecde
Kaira <voody2506@gmail.com>
2021-04-19 00:11:58 +0600

    added public struct

--- World/Components/InstantiateComponent.cs
+++ World/Components/InstantiateComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-struct InstantiateComponent {
+public struct InstantiateComponent {
     public GameObject gameObject;
     public Vector3 position;
     public Quaternion rotation;
