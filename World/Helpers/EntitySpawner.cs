140693720ea28f486e97b53eec1021083af3525d
Kaira <voody2506@gmail.com>
2021-04-19 01:10:14 +0600

    Added Entity Spawner

---
+++ World/Helpers/EntitySpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Leopotam.Ecs;
+using UnityEngine;
+
+public static class EntitySpawner
+{
+    public static void Instatiate(
+        GameObject gameObject,
+        Vector3 position,
+        Quaternion rotation,
+        EcsWorld _world
+    )
+    {
+        EcsEntity entity = _world.NewEntity();
+        var spawner =
+            new InstantiateComponent() { gameObject = gameObject, position = position, rotation = rotation };
+        entity.Replace (spawner);
+    }
+}
