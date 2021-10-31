14a4822d226265dde76c418f08d41a9529380c0c
voody2506 <voody2506@gmail.com>
2021-10-31 20:42:58 +0600

    Fixed Unity IL2CPP Build Error

--- Runtime/World/Systems/WorldInitSystem.cs
+++ Runtime/World/Systems/WorldInitSystem.cs
@@ -84,21 +84,3 @@ namespace Voody.UniLeo
         }
     }
 }
-
-#if ENABLE_IL2CPP
-// Unity IL2CPP performance optimization attribute.
-namespace Unity.IL2CPP.CompilerServices {
-    enum Option {
-        NullChecks = 1,
-        ArrayBoundsChecks = 2
-    }
-
-    [AttributeUsage (AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
-    class Il2CppSetOptionAttribute : Attribute {
-        public Option Option { get; private set; }
-        public object Value { get; private set; }
-
-        public Il2CppSetOptionAttribute (Option option, object value) { Option = option; Value = value; }
-    }
-}
-#endif
