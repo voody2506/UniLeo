554a9ad3a4a0edd88ef60dc48f53fa95a1297614
Kaira <voody2506@gmail.com>
2021-11-10 00:17:30 +0600

    Added fixes

--- Runtime/MonoHelpers/ConvertToEntity.cs
+++ Runtime/MonoHelpers/ConvertToEntity.cs
@@ -8,21 +8,40 @@ namespace Voody.UniLeo
     public enum ConvertMode
     {
         ConvertAndInject,
-        ConvertAndDestroy
+        ConvertAndDestroy,
+        ConvertAndSaveEntity
     }
     public class ConvertToEntity : MonoBehaviour
     {
         public ConvertMode convertMode;
+        private EcsEntity? entity;
         private void Start()
         {
             var world = WorldHandler.GetWorld();
-            Debug.Log(world);
             if (world != null)
             {
                 var entity = world.NewEntity();
                 var instantiateComponent = new InstantiateComponent() { gameObject = gameObject };
                 entity.Replace(instantiateComponent);
             }
+        }
+
+        public EcsEntity? TryGetEntity()
+        {
+            if (entity.HasValue)
+            {
+                if (entity.Value.IsAlive())
+                {
+                    return entity.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public void Set(EcsEntity entity)
+        {
+            this.entity = entity;
         }
     }
 }
